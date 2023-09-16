336a8931efadf7312649f5d869409e6e7335b9f1
Prahlad <prahladjngp@gmail.com>
2023-09-16 15:01:36 +0530

    img to Image

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,6 @@ import Contact from "@/components/Contact";
 import Features from "@/components/Features";
 import Footer from "@/components/Footer";
 import Hero from "@/components/Hero";
-import Image from "next/image";
 
 export default function Home() {
   return (
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -100,7 +100,7 @@ export default function Hero() {
         </div>
         <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mt-0 lg:mr-0 lg:max-w-none lg:flex-none xl:ml-32">
           <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
-            <img
+            <Image
               src="/TMDocsLanding.jpg"
               alt="App screenshot"
               width={2432}
